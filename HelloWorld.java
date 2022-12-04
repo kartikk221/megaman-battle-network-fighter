e9e565a568471287ee0edd2f45d5b9e46438c9bc
kartikk221 <kartikk221@yahoo.com>
2022-12-04 13:47:53 -0500

    Setup JavaFX with a build bash

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,4 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -11,17 +9,12 @@ public class HelloWorld extends Application {
         launch(args);
     }
     
-    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        btn.setOnMouseClicked(e -> {
+            System.out.println("Hello World!");
         });
         
         StackPane root = new StackPane();
